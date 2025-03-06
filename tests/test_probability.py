9376bffe5d70ca9248c8b8adda49ec2e43ff51ff
Mario VB <mario.villasante@islas.org.mx>
2025-03-06 16:52:35 +0000

    ♻️  Refactor: test extracted prob function

--- tests/test_probability.py
+++ tests/test_probability.py
@@ -1,8 +1,15 @@
-from thinkbayes.probability import get_fraction_of_bankers
+from thinkbayes.probability import get_fraction_of_bankers, prob
 import pandas as pd
 
 
+gss = pd.read_csv("tests/data/gss_bayes.csv")
+
+
 def test_fraction_of_bankers():
-    gss = pd.read_csv("tests/data/gss_bayes.csv")
     obtained = get_fraction_of_bankers(gss)
     assert obtained < 1
+
+
+def test_prob_female():
+    female = gss["sex"] == 2
+    assert prob(female) == 0.5378575776019476
