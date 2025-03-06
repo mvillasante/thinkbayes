b36a2f1cc17dbbdc4d764a65a639a822cee5e401
Mario VB <mario.villasante@islas.org.mx>
2025-03-06 17:15:54 +0000

    ✅ Pass tests

--- thinkbayes/probability.py
+++ thinkbayes/probability.py
@@ -1,7 +1,11 @@
-def get_fraction_of_bankers(data):
-    banker = data["indus10"] == 6870
-    return prob(banker)
+def conditional(a, given):
+    return prob(a[given])
 
 
 def prob(a):
     return a.mean()
+
+
+def get_fraction_of_bankers(data):
+    banker = data["indus10"] == 6870
+    return prob(banker)
