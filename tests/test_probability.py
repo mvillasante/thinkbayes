b579e246913088cd29e53d2a709471713ff48b57
Mario VB <mario.villasante@islas.org.mx>
2025-03-06 17:08:53 +0000

    ♻️  Refactor: conditional probability

--- tests/test_probability.py
+++ tests/test_probability.py
@@ -5,21 +5,41 @@ import pandas as pd
 gss = pd.read_csv("tests/data/gss_bayes.csv")
 
 
+banker = gss["indus10"] == 6870
+
+
 def test_fraction_of_bankers():
     obtained = get_fraction_of_bankers(gss)
     assert obtained == 0.014769730168391155
-    bankers = gss["indus10"] == 6870
-    assert prob(bankers) == 0.014769730168391155
+    assert prob(banker) == 0.014769730168391155
+
+
+party_index = 1
+democrat = gss.partyid <= party_index
+
+polviews_index = 3
+liberal = gss["polviews"] <= polviews_index
 
 
 def test_prob_features():
     female = gss["sex"] == 2
     assert prob(female) == 0.5378575776019476
 
-    polviews_index = 3
-    liberal = gss["polviews"] <= polviews_index
     assert prob(liberal) == 0.27374721038750255
 
-    polviews_index = 1
-    democrat = gss.partyid <= polviews_index
     assert prob(democrat) == 0.3662609048488537
+
+
+def test_conjuntion():
+    """
+    probability that a respondent is a banker and a Democrat
+    """
+    assert prob(banker & democrat) == 0.004686548995739501
+
+
+def test_conditional_probability():
+    """
+    probability that a respondent is a Democrat given that they are a liberal
+    """
+    selected = democrat[liberal]
+    assert prob(selected) == 0.5206403320240125
