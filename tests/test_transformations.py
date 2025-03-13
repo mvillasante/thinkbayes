a7edb2c5c212d70845a3c8fb09974dcee428f9bf
Mario Villasante <mario.villasante@islas.org.mx>
2025-03-13 08:43:31 -0700

    🔥⚰️ remove unused files

--- tests/test_transformations.py
+++
@@ -1,9 +0,0 @@
-from thinkbayes.dummy import add_offset
-
-
-def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = add_offset(augend, addend)
-    assert expected == obtained
